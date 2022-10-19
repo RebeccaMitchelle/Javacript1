87a4cdacb1284c4def3d0f746a933342fedc9a86
RebeccaMitchelle <rebeccaamitchelle@gmail.com>
2022-10-19 10:37:48 +0300

    Javascript1

---
+++ Javascript1.js
@@ -0,0 +1,37 @@
+/**
+ * Switch
+ * This is an assignment to execute a switch including the operation of an elevator.
+ * 
+ * 
+ */
+
+//switch
+let elevator=2
+
+switch(elevator){
+    case 1:
+        console.log("This is the first level")
+        break
+case 2:
+    console.log("This is the second level.")
+break
+case 3:
+    console.log("This is the third level.")
+    break
+    default:
+        console.log("Out of options.")
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
